a3d1ed73e74e4ce0fa3dea9d5f87950aa17f7790
Jesper Lundberg <jeppzer99@gmail.com>
2023-09-19 13:44:46 +0200

    Added querystings to pagination get

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,8 @@ class PostController extends Controller
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->simplePaginate(9),
+            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))
+            ->simplePaginate(9)->withQueryString(),
         ]);
     }
 
